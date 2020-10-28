58eefb63f500d7a7e5356cf9586e0c7cf9a15e34
Szymon <szymonjuraszek777@gmail.com>
2020-10-28 20:18:39 +0100

    Increase communication time

--- globalConfig.ts
+++ globalConfig.ts
@@ -1,4 +1,4 @@
 // okolo 47 requestow na sekunde
 export const MESSAGE_FREQUENCY = 21;
-export const COMMUNICATION_TIME = 80000;
+export const COMMUNICATION_TIME = 200000;
 export const URL = 'https://83.229.84.77:8080';
