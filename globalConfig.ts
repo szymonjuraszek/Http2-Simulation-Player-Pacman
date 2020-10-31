d82cecaa74803ce707735559458909130ed5a5bc
Szymon <szymonjuraszek777@gmail.com>
2020-10-31 21:06:41 +0100

    Add sending message for 10 millis

--- globalConfig.ts
+++ globalConfig.ts
@@ -1,4 +1,4 @@
 // okolo 47 requestow na sekunde
-export const MESSAGE_FREQUENCY = 21;
+export const MESSAGE_FREQUENCY = 10;
 export const COMMUNICATION_TIME = 200000;
 export const URL = 'https://83.229.84.77:8080';
