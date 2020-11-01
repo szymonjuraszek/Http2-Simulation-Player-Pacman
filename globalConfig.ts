eda536f191ee1e7b18e965298262ea18f04adf6c
Szymon <szymonjuraszek777@gmail.com>
2020-11-01 18:17:36 +0100

    15

--- globalConfig.ts
+++ globalConfig.ts
@@ -1,5 +1,5 @@
 // okolo 47 requestow na sekunde
-export const MESSAGE_FREQUENCY = 21;
+export const MESSAGE_FREQUENCY = 15;
 export const COMMUNICATION_TIME = 200000;
 export const URL = 'https://83.229.84.77:8080';
 // export const URL = 'https://localhost:8080';
